188d858bb02f8709266c3061267bf24441124446
Joshua Carroll <JoshuaCarroll@users.noreply.github.com>
2021-02-15 15:30:23 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host_name = "localhost";
+
+// Database username
+$user_name = "4fOvO3IUXSNCKmNCMBSYNw==";
+
+// Database password
+$password = "W100DW99ykVZNQ+Rx1HM5g==";
+
+// Database schema name
+$database = "theFile";
+?>
