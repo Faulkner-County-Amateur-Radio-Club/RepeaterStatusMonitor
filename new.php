2f70f49ff7362df446bc63ac58d882ac32191280
George Carroll <ghcarrolljr@gmail.com>
2021-02-19 08:57:17 -0600

    test for problem

--- new.php
+++ new.php
@@ -130,7 +130,7 @@ class Repeater {
 		}
 	}
     function RetrevePrevious() {
-        $sql = "SELECT id , State,  Voltage, LastHeard FROM PreviousState";
+       /* $sql = "SELECT id , State,  Voltage, LastHeard FROM PreviousState";
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
             // output data of each row
@@ -144,7 +144,7 @@ class Repeater {
         else {
             echo "0 results";
         }
-        
+        */
     }
 }
 
