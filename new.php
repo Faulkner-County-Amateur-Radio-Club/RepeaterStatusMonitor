aed92d8b645adebe87f68a3c9895091601bfe5c6
George Carroll <ghcarrolljr@gmail.com>
2021-02-19 11:12:52 -0600

    Update new.php

--- new.php
+++ new.php
@@ -1,4 +1,5 @@
 <?php
+include "./config.php";
 function GetPrevious() {
     $query = "";
     // Create connection
@@ -55,14 +56,14 @@ class Repeater {
 
 	function __construct($name, $description, $frequency, $telemetryVoltageChannel, $telemetryGridPowerStatusChannel, $telemetryGridPowerThreshold, $telemetryTempuratureChannel) {
 		
-		include "./config.php";
-        	$this->recipient = $recipient;
-            $this->sendFrom = $sendFrom;
-            $this->host_name = $host_name;
-            $this->user_name = $user_name;
-            $this->password = $password;
-            $this->database = $database;
-            $this->recipients = $recipients;
+	//	include "./config.php";
+    //    	$this->recipient = $recipient;
+    //        $this->sendFrom = $sendFrom;
+    //        $this->host_name = $host_name;
+    //        $this->user_name = $user_name;
+    //        $this->password = $password;
+    //        $this->database = $database;
+    //        $this->recipients = $recipients;
 		
 		$this->name = $name;
 		$this->description = $description;
