6480557f438975e32f22a06901433584e28279d4
Joshua Carroll <joshcarroll21@gmail.com>
2021-02-18 19:16:44 -0600

    Added proper HTML

--- new.php
+++ new.php
@@ -100,12 +100,30 @@ class Repeater {
 }
 
 $w5auu1 = new Repeater("W5AUU-1", "146.97 repeater", "146.97", 1, 5, 50, 2);
-$w5auu1->toString();
-
 $w5auu2 = new Repeater("W5AUU-2", "OEM repeater shack", "147.03", 1, 5, 50, 2);
-$w5auu2->toString();
-
 $w5auu3 = new Repeater("W5AUU-3", "Greenbriar repeater shack", "146.625", 1, 3, 100, 2);
-$w5auu3->toString();
 
+?><html>
+	<head>
+		<title>Repeater Status Monitor</title>
+		<style>
+			span { 
+				color: darkred; 
+				animation: blinker 1s linear infinite;
+			}
+
+			@keyframes blinker {
+			  50% {
+				opacity: 0;
+			  }
+			}
+		</style>
+	</head>
+	<body>
+<?php 
+$w5auu1->toString();
+$w5auu2->toString();
+$w5auu3->toString();
 ?>
+	</body>
+</html>
