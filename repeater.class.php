1c6db7b3d341499a5f445c54bfd41c84db499acc
Joshua Carroll <joshcarroll21@gmail.com>
2021-02-23 11:43:02 -0600

    Fixed cache function

--- repeater.class.php
+++ repeater.class.php
@@ -42,31 +42,30 @@ class Repeater {
 		$this->loadData();
 	}
 	function getJson($url) {
-		// cache files are created like cache/abcdef123456...
-		$cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
+			// cache files are created like cache/abcdef123456...
+			$cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
 
-		if (file_exists($cacheFile)) {
-			$fh = fopen($cacheFile, 'r');
-			$cacheTime = trim(fgets($fh));
+			if (file_exists($cacheFile)) {
+					$fh = fopen($cacheFile, 'r');
+					$cacheTime = trim(fgets($fh));
 
-			// if data was cached recently, return cached data
-			if ($cacheTime > strtotime('-5 minutes')) {
-				return fread($fh);
+					// if data was cached recently, return cached data
+					if ($cacheTime > strtotime('-5 minutes')) {
+							return fread($fh,filesize("$cacheFile"));
+					}
+
+					// else delete cache file
+					fclose($fh);
+					unlink($cacheFile);
 			}
 
-			// else delete cache file
+			$json = file_get_contents($url);
+
+			$fh = fopen($cacheFile, 'w');
+			fwrite($fh, time() . "\n");
+			fwrite($fh, $json);
 			fclose($fh);
-			unlink($cacheFile);
-		}
-
-		$json = file_get_contents($url);
-
-		$fh = fopen($cacheFile, 'w');
-		fwrite($fh, time() . "\n");
-		fwrite($fh, $json);
-		fclose($fh);
-
-		return $json;
+			return $json;
 	}
 	function loadData() {
 		$this->jsonUrl = "https://api.aprs.fi/api/get?name=" . $this->name . "&what=loc&apikey=100665.Mj8HjUvXqEHYjrV6&format=json";
