06d982872fd42093c093e2b8d3c924352b4ce0b3
Joshua Carroll <joshcarroll21@gmail.com>
2021-02-23 12:49:04 -0600

    Corrected time zone

--- json.php
+++ json.php
@@ -12,7 +12,7 @@
 			$this->w5auu2 = new Repeater("W5AUU-2", "OEM repeater shack", "147.03", 1, 5, 50, 2);
 			$this->w5auu3 = new Repeater("W5AUU-3", "Greenbrier repeater shack", "146.625", 1, 3, 100, 2);
 			
-			date_default_timezone_set("America/New_York");
+			date_default_timezone_set("America/Chicago");
 			$this->reportTime = date("d/m/Y h:i:sa");
 		}
 	}
